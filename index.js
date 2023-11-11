586ffcb5c3cdd7fc9231114f6b80f57e90b2fdc5
Ashutosh Kumar Maurya <akmashh62@gmail.com>
2023-11-11 16:42:16 +0530

    Adding templating engine

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express')
 const path = require('path')
-
 const app = new express()
+const expressEdge = require('express-edge')
 
 app.use(express.static('public'))
+app.use(expressEdge)
+app.set('views', `${__dirname}/views`)
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')  //render the about.edge file which is a html format
 })
 
 app.get('/post', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 })
 
 app.get('/contact', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 
 app.listen(4000, ()=>{
